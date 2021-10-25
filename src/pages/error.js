4a290ac953195cd635d140dc47d781c48e388f5d
Smart8878 <68674617+Smart8878@users.noreply.github.com>
2021-10-25 11:01:01 +0530

    Update error.js

--- src/pages/error.js
+++ src/pages/error.js
@@ -4,12 +4,14 @@ import Footer from "../components/footer"
 
 const Error = () => {
     return (
-        <div>
+       <div style={{ height: "100%" }}>
             <Header />
-            <div style={{height:"345px",marginTop:"50px",marginLeft:"100px"}}>
+            <div style={{ height: "345px", marginTop: "50px", marginLeft: "100px" }}>
                 <label>Oops, something went wrong..</label>
             </div>
-            <Footer />
+            <div style={{ position: "absolute", left: "0", right: "0", bottom: "0" }}>
+                <Footer />
+            </div>
         </div>
     )
 
